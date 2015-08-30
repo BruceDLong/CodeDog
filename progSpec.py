174a9fb2672b4eb55e75f90093924036adaf7418
Bruce Long <qstream@gmail.com>
2015-08-30 14:26:43 -0600

    Changes to progSpec construction functions.

--- progSpec.py
+++ progSpec.py
@@ -3,7 +3,7 @@
 
 import re
 
-def addObject(objSpecs, objectNameList, name, objTags):
+def addObject(objSpecs, objectNameList, name):
     objectNameList.append(name)
     if(name in objSpecs):
         print "The struct '", name, "' is being added but already exists."
@@ -11,8 +11,13 @@ def addObject(objSpecs, objectNameList, name, objTags):
     objSpecs[name]={'name':name, "attrList":[], "attr":{}, "fields":[]}
     print "ADDED STRUCT: ", name
 
+def addObjTags(objSpecs, objectName, objTags):
+    objSpecs[objectName]["tags"]=objtags
+    print "    ADDED Tags to object.\t"
+
+
 def addField(objSpecs, objectName, kindOfField, fieldType, fieldName):
-    if (kindOfField=="var" or kindOfField=="rPtr" kindOfField=="sPtr" or kindOfField=="uPtr"):
+    if (kindOfField=="var" or kindOfField=="rPtr" or kindOfField=="sPtr" or kindOfField=="uPtr"):
         objSpecs[objectName]["fields"].append({'kindOfField':kindOfField, 'fieldType':fieldType, 'fieldName':fieldName})
     else:
         print "When adding a Field to ", objectName, ", invalid field type: ", kindOfField,"."
@@ -37,47 +42,22 @@ def addFunc(objSpecs, objectName, funcName, returnType, argList, funcBody):
     objSpecs[objectName]["fields"].append({'kindOfField':'func', 'funcText':funcText, 'returnType':returnType, 'fieldName':funcName})
     print "    ADDED FUNCTION:\t", funcName
 
-def FillStructFromText(objSpecs, objectNameList, objectName, structDefString):
-    print "POPULATING STRUCT ", objectName, " FROM TEXT..."
+def addActionToSeq(objSpecs, objectName, funcName, ):
 
-    #Split struct body
-    structBodyText=structDefString
-    items=None
-    while(1):
-        fieldTypeM=re.match("\s*(\w+)\s*:\s*", structBodyText)
-        if fieldTypeM is None:
-            break
-        fieldType=fieldTypeM.group(1);
-        #print fieldType
-        if(fieldType=="flag"):
-            items=re.match("\s*(\w+)\s*:\s*(\w+)", structBodyText)
-            addFlag(objSpecs, objectNames, items.group(2))
-        elif fieldType=="mode":
-            items=re.match("\s*(\w+)\s*:\s*(\w+)\s*\[([\w\s,]*)\]", structBodyText)
-            #print "<<<<<<<"+items.group(3)+">>>>>>>>>"
-            fieldsName=items.group(2)
-            enumList=re.findall("\w+", items.group(3))
-            addMode(objSpecs, objectNames, fieldsName, enumList)
-        elif (fieldType=="var"):
-            items=re.match("\s*(\w+)\s*:\s*(\w+)\s*([\w\s<> \*,\[\]]*);", structBodyText)
-            fieldsType=items.group(2)
-            fieldsName=items.group(3)
-            addField(objSpecs, objectNames, "var", fieldsType, fieldsName)
-        elif fieldType=="ptr":
-            items=re.match("\s*(\w+)\s*:\s*(\w+)\s*(\w*)", structBodyText)
-            fieldsType=items.group(2)
-            fieldsName=items.group(3)
-            addField(objSpecs, objectNames, "ptr", fieldsType, fieldsName)
-        elif fieldType=="dblList":
-            items=re.match("\s*(\w+)\s*:\s*(\w+)\s*(\w*)", structBodyText)
-            fieldsType=items.group(2)
-            fieldsName=items.group(3)
-            addField(objSpecs, objectNames, "dblList", fieldsType, fieldsName)
-        elif fieldType=="func":
-            items=re.match("\s*(\w+)\s*:\s*(.+?)\s*:\s*(.+?)\s*END", structBodyText, re.DOTALL)
-            returnType=items.group(2)
-            funcsText=items.group(3)
-            fieldsName="FUNC"
-            addFunc(objSpecs, objectNames, fieldsName, returnType, funcsText)
+    print "        ADDED Action to ", objName, ".", funcName, ".\n"
 
-        structBodyText=structBodyText[items.end():]
+def addSequenceToFunc(objSpecs, objectName, funcName, ):
+
+    print "        ADDED Sequence to ", objName, ".", funcName, ".\n"
+
+def addRepetitionToFunc(objSpecs, objectName, funcName, ):
+
+    print "        ADDED Repetition to ", objName, ".", funcName, ".\n"
+
+def addLocalVarToFunc(objSpecs, objectName, funcName, ):
+
+    print "        ADDED LocalVar to ", objName, ".", funcName, ".\n"
+
+def addCommentToFunc(objSpecs, objectName, funcName, ):
+
+    print "        ADDED Comment to ", objName, ".", funcName, ".\n"
