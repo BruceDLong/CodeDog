c7b9bce9f1e60aaa964918a245ca2279dadf939d
TiffanyLawrence <ktiffanywalker@gmail.com>
2016-06-17 13:14:26 -0600

    Adding Lib_Java.py

---
+++ Lib_Java.py
@@ -0,0 +1,25 @@
+#///////// Add routines for Java
+
+import progSpec
+import codeDogParser
+
+def use(objects, buildSpec, tags, platform):
+    print "USING Java"
+
+    CODE="""
+        
+    """
+    codeDogParser.AddToObjectFromText(objects[0], objects[1], CODE )
+
+    #APP_UTILITY_CODE = createUtilityFunctions()
+    #MENU_BAR_CODE = createMenubar()
+    #MAIN_APP_CODE = createMainAppArea()
+    #STATUS_BAR_CODE=createStatusBar()
+
+
+    #GLOBAL_CODE="""
+        
+    #""" % (APP_UTILITY_CODE, MENU_BAR_CODE, MAIN_APP_CODE, STATUS_BAR_CODE)
+    
+    
+    #codeDogParser.AddToObjectFromText(objects[0], objects[1], GLOBAL_CODE )
