7a4c993c43039bd640e4b70f6721556f57054122
TiffanyLawrence <ktiffanywalker@gmail.com>
2016-07-17 20:54:37 -0600

    re-adding GUI_toolkit

---
+++ pattern_GUI_Toolkit.py
@@ -0,0 +1,26 @@
+#/////////////////  Routines For Graphics, Text, Audio, User-I/O, fileMgmt, etc.
+
+import progSpec
+import codeDogParser
+
+
+def apply(objects, tags):
+    progSpec.appendToStringTagValue(tags, 'initCode', 'static_gui_tk.GUI_Init()')
+    progSpec.appendToStringTagValue(tags, 'runCode', 'static_gui_tk.GUI_Run()')
+    progSpec.appendToStringTagValue(tags, 'deinitCode', 'static_gui_tk.GUI_Deinit()')
+
+    # Based on tags, choose a set of libraries and mark-activate them.
+    progSpec.setFeaturesNeeded(tags, ['GUI_ToolKit', 'Mouse', 'Keyboard', 'Audio', 'Locale', 'Unicode'], 'GUI_PATTERN')
+
+    # Provide wrapper commands
+
+    # Make a class with init, event-loop, deInit (Can be activated from main.)
+    title=progSpec.searchATagStore(tags, 'Title')[0]
+    GUI_TK_code = """
+    struct GLOBAL {
+        const string: title <- "%s"
+    }
+
+    """ % title
+
+    codeDogParser.AddToObjectFromText(objects[0], objects[1], GUI_TK_code )
