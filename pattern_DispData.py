374c321f69e730be2685ecdc27eb7c6cb6b950f6
Bruce Long <qstream@gmail.com>
2017-10-31 20:13:19 -0600

    Making dataDisp object oriented

--- pattern_DispData.py
+++ pattern_DispData.py
@@ -6,125 +6,127 @@ from progSpec import cdlog, cdErr
 
 import pattern_GenSymbols
 
-thisPatternAlreadyUsedOnce=False
-
 classesToProcess=[]
 classesEncoded={}
 
+class structProcessor:
+
 #---------------------------------------------------------------  TEXT GEN
-def displayTextFieldAction(label, fieldName, field, fldCat):
-    global classesToProcess
-    global classesEncoded
-    valStr=''
-    if(fldCat=='int' or fldCat=='double'):
-        valStr='toString('+fieldName+')'
-    elif(fldCat=='string' or fldCat=='char'):
-        valStr= "'"+fieldName+"'"
-    elif(fldCat=='flag' or fldCat=='bool'):
-        valStr='dispBool(('+fieldName+')!=0)'
-    elif(fldCat=='mode'):
-        valStr='toString('+fieldName+')'  #fieldName+'Strings['+fieldName+'] '
-    elif(fldCat=='struct'):
-        valStr=fieldName+'.dump(indent+"|   ")\n'
+class toString:
 
-        structTypeName=field['typeSpec']['fieldType'][0]
-        if not(structTypeName in classesEncoded):
-            #print "TO ENDODE:", structTypeName
-            classesEncoded[structTypeName]=1
-            classesToProcess.append(structTypeName)
-    if(fldCat=='struct'):
-        S="    "+'print(indent, dispFieldAsText("'+label+'", 15), "\\n")\n    '+valStr+'\n    print("\\n")\n'
-    else:
-        S="    "+'print(indent, dispFieldAsText("'+label+'", 15), '+valStr+', "\\n")\n'
-    return S
-
-def encodeFieldText(fieldName, field, fldCat):
-    S=""
-    if fldCat=='func': return ''
-    typeSpec=field['typeSpec']
-    if 'arraySpec' in typeSpec and typeSpec['arraySpec']!=None:
-        innerFieldType=typeSpec['fieldType']
-        #print "ARRAYSPEC:",innerFieldType, field
-        fldCatInner=progSpec.innerTypeCategory(innerFieldType)
-        calcdName=fieldName+'["+toString(_item_key)+"]'
-        S+="    withEach _item in "+fieldName+":{\n"
-        S+="        "+displayTextFieldAction(calcdName, '_item', field, fldCatInner)+"    }\n"
-    else: S+=displayTextFieldAction(fieldName, fieldName, field, fldCat)
-    if progSpec.typeIsPointer(typeSpec):
-        T ="    if("+fieldName+' == NULL){print('+'indent, dispFieldAsText("'+fieldName+'", 15)+"NULL\\n")}\n'
-        T+="    else{\n    "+S+"    }\n"
-        S=T
-    return S
-
-#---------------------------------------------------------------  DRAW GEN
-
-def getDashDeclAndUpdateCode(owner, fieldLabel, fieldRef, fieldName, field, skipFlags, indent):
-    global classesToProcess
-    global classesEncoded
-    [structText, updateFuncText, setPosFuncText, drawFuncText, handleClicksFuncText]=['', '', '', '', '']
-    typeSpec=field['typeSpec']
-    fldCat=progSpec.fieldsTypeCategory(typeSpec)
-    if fldCat=='func': return ['', '', '', '', '']
-    if progSpec.typeIsPointer(typeSpec) and skipFlags != 'skipPtr':  # Header for a POINTER
-        fieldName+='Ptr'
-        innerUpdateFuncStr = 'data.'+fieldRef+'.mySymbol(data.'+fieldRef+')'
-        updateFuncText+="        "+fieldName+'.update('+fieldLabel+', '+innerUpdateFuncStr+', isNull('+innerUpdateFuncStr+'))\n'
-        structText += "    "+owner+" ptrToItem: "+fieldName+"\n"
-
-    elif 'arraySpec' in typeSpec and typeSpec['arraySpec']!=None and skipFlags != 'skipLists': # Header and items for LIST
-        innerUpdateFuncStr = '"Size:"+toString(data.'+fieldName+'.size())'
-        updateFuncText="        "+fieldName+'.update('+fieldLabel+', '+innerUpdateFuncStr+', isNull('+innerUpdateFuncStr+'))\n'
-         ## Now push each item
-        innerFieldType=typeSpec['fieldType']
-        #print "ARRAYSPEC:",innerFieldType, field
-        fldCatInner=progSpec.innerTypeCategory(innerFieldType)
-        newFieldRef=fieldName+'[_item_key]'
-        newFieldLabel='"'+fieldName+'["+toString(_item_key)+"]"'
-        updateFuncText+="\n        withEach _item in data."+fieldName+":{\n"
-        [innerStructText, innerUpdateFuncText, innerDrawFuncText, innerSetPosFuncText, innerHandleClicksFuncText]=getDashDeclAndUpdateCode('our', newFieldLabel, newFieldRef, 'newItem', field, 'skipLists', '    ')
-        updateFuncText+=innerStructText+'\n        Allocate(newItem)\n'+innerUpdateFuncText
-        updateFuncText+="\n        "+fieldName+'.updatePush(newItem)'
-        updateFuncText+='\n        }\n'
-        structText += "    "+owner+" listOfItems: "+fieldName+"\n"
-    elif(fldCat=='struct'):  # Header for a STRUCT
-        updateFuncText="        "+fieldName+'.update('+fieldLabel+', ">", data.'+fieldRef+')\n'
-        structTypeName=field['typeSpec']['fieldType'][0]
-        structText += "    "+owner+" display_"+structTypeName+': '+fieldName+"\n"
-
-
-        # Add new classname to a list so it can be encoded.
-        if not(structTypeName in classesEncoded):
-            classesEncoded[structTypeName]=1
-            classesToProcess.append(structTypeName)
-
-    else:   # Display field for a BASIC TYPES
+    def displayTextFieldAction(label, fieldName, field, fldCat):
+        global classesToProcess
+        global classesEncoded
         valStr=''
         if(fldCat=='int' or fldCat=='double'):
-            valStr='toString(data.'+fieldName+')'
+            valStr='toString('+fieldName+')'
         elif(fldCat=='string' or fldCat=='char'):
-            valStr= '"\'"+'+'data.'+fieldName+'+"\'"'
+            valStr= "'"+fieldName+"'"
         elif(fldCat=='flag' or fldCat=='bool'):
-            valStr='dispBool((data.'+fieldName+')!=0)'
+            valStr='dispBool(('+fieldName+')!=0)'
         elif(fldCat=='mode'):
-            valStr= fieldRef+'Strings[data.'+fieldName+'] '
+            valStr='toString('+fieldName+')'  #fieldName+'Strings['+fieldName+'] '
+        elif(fldCat=='struct'):
+            valStr=fieldName+'.dump(indent+"|   ")\n'
 
-        updateFuncText="        "+fieldName+'.update(90, 150, '+fieldLabel+', '+valStr+', isNull('+valStr+'))\n'
-        structText += "    "+owner+" dataField: "+fieldName+"\n"
+            structTypeName=field['typeSpec']['fieldType'][0]
+            if not(structTypeName in classesEncoded):
+                #print "TO ENDODE:", structTypeName
+                classesEncoded[structTypeName]=1
+                classesToProcess.append(structTypeName)
+        if(fldCat=='struct'):
+            S="    "+'print(indent, dispFieldAsText("'+label+'", 15), "\\n")\n    '+valStr+'\n    print("\\n")\n'
+        else:
+            S="    "+'print(indent, dispFieldAsText("'+label+'", 15), '+valStr+', "\\n")\n'
+        return S
 
-    drawFuncText  ="        "+fieldName+'.draw(cr)\n'
-    setPosFuncText += '''        if(!<fieldName>.isNullLike or displayMode==fullDisplay){
-            <fieldName>.setPos(x,y,extC)
-            extC <- <fieldName>.extC
-            y <- y + <fieldName>.height
-            extX <- max(extX, <fieldName>.extX)
-            extY <- max(extY, <fieldName>.extY)
-            width<- max(width, <fieldName>.width)
-            <fieldName>.isHidden<-false
-        } else {<fieldName>.isHidden<-true}
-'''.replace('<fieldName>', fieldName)
-  #  updateFuncText+='        if(crntWidth<'+fieldName+'.width){crntWidth <- '+fieldName+'.width}'
-    handleClicksFuncText = '            '+fieldName+'.primaryClick(event)'
-    return [structText, updateFuncText, drawFuncText, setPosFuncText, handleClicksFuncText]
+    def encodeFieldText(fieldName, field, fldCat):
+        S=""
+        if fldCat=='func': return ''
+        typeSpec=field['typeSpec']
+        if 'arraySpec' in typeSpec and typeSpec['arraySpec']!=None:
+            innerFieldType=typeSpec['fieldType']
+            #print "ARRAYSPEC:",innerFieldType, field
+            fldCatInner=progSpec.innerTypeCategory(innerFieldType)
+            calcdName=fieldName+'["+toString(_item_key)+"]'
+            S+="    withEach _item in "+fieldName+":{\n"
+            S+="        "+displayTextFieldAction(calcdName, '_item', field, fldCatInner)+"    }\n"
+        else: S+=displayTextFieldAction(fieldName, fieldName, field, fldCat)
+        if progSpec.typeIsPointer(typeSpec):
+            T ="    if("+fieldName+' == NULL){print('+'indent, dispFieldAsText("'+fieldName+'", 15)+"NULL\\n")}\n'
+            T+="    else{\n    "+S+"    }\n"
+            S=T
+        return S
+
+#---------------------------------------------------------------  DRAW GEN
+class drawStruct:
+    def getDashDeclAndUpdateCode(owner, fieldLabel, fieldRef, fieldName, field, skipFlags, indent):
+        global classesToProcess
+        global classesEncoded
+        [structText, updateFuncText, setPosFuncText, drawFuncText, handleClicksFuncText]=['', '', '', '', '']
+        typeSpec=field['typeSpec']
+        fldCat=progSpec.fieldsTypeCategory(typeSpec)
+        if fldCat=='func': return ['', '', '', '', '']
+        if progSpec.typeIsPointer(typeSpec) and skipFlags != 'skipPtr':  # Header for a POINTER
+            fieldName+='Ptr'
+            innerUpdateFuncStr = 'data.'+fieldRef+'.mySymbol(data.'+fieldRef+')'
+            updateFuncText+="        "+fieldName+'.update('+fieldLabel+', '+innerUpdateFuncStr+', isNull('+innerUpdateFuncStr+'))\n'
+            structText += "    "+owner+" ptrToItem: "+fieldName+"\n"
+
+        elif 'arraySpec' in typeSpec and typeSpec['arraySpec']!=None and skipFlags != 'skipLists': # Header and items for LIST
+            innerUpdateFuncStr = '"Size:"+toString(data.'+fieldName+'.size())'
+            updateFuncText="        "+fieldName+'.update('+fieldLabel+', '+innerUpdateFuncStr+', isNull('+innerUpdateFuncStr+'))\n'
+             ## Now push each item
+            innerFieldType=typeSpec['fieldType']
+            #print "ARRAYSPEC:",innerFieldType, field
+            fldCatInner=progSpec.innerTypeCategory(innerFieldType)
+            newFieldRef=fieldName+'[_item_key]'
+            newFieldLabel='"'+fieldName+'["+toString(_item_key)+"]"'
+            updateFuncText+="\n        withEach _item in data."+fieldName+":{\n"
+            [innerStructText, innerUpdateFuncText, innerDrawFuncText, innerSetPosFuncText, innerHandleClicksFuncText]=getDashDeclAndUpdateCode('our', newFieldLabel, newFieldRef, 'newItem', field, 'skipLists', '    ')
+            updateFuncText+=innerStructText+'\n        Allocate(newItem)\n'+innerUpdateFuncText
+            updateFuncText+="\n        "+fieldName+'.updatePush(newItem)'
+            updateFuncText+='\n        }\n'
+            structText += "    "+owner+" listOfItems: "+fieldName+"\n"
+        elif(fldCat=='struct'):  # Header for a STRUCT
+            updateFuncText="        "+fieldName+'.update('+fieldLabel+', ">", data.'+fieldRef+')\n'
+            structTypeName=field['typeSpec']['fieldType'][0]
+            structText += "    "+owner+" display_"+structTypeName+': '+fieldName+"\n"
+
+
+            # Add new classname to a list so it can be encoded.
+            if not(structTypeName in classesEncoded):
+                classesEncoded[structTypeName]=1
+                classesToProcess.append(structTypeName)
+
+        else:   # Display field for a BASIC TYPES
+            valStr=''
+            if(fldCat=='int' or fldCat=='double'):
+                valStr='toString(data.'+fieldName+')'
+            elif(fldCat=='string' or fldCat=='char'):
+                valStr= '"\'"+'+'data.'+fieldName+'+"\'"'
+            elif(fldCat=='flag' or fldCat=='bool'):
+                valStr='dispBool((data.'+fieldName+')!=0)'
+            elif(fldCat=='mode'):
+                valStr= fieldRef+'Strings[data.'+fieldName+'] '
+
+            updateFuncText="        "+fieldName+'.update(90, 150, '+fieldLabel+', '+valStr+', isNull('+valStr+'))\n'
+            structText += "    "+owner+" dataField: "+fieldName+"\n"
+
+        drawFuncText  ="        "+fieldName+'.draw(cr)\n'
+        setPosFuncText += '''        if(!<fieldName>.isNullLike or displayMode==fullDisplay){
+                <fieldName>.setPos(x,y,extC)
+                extC <- <fieldName>.extC
+                y <- y + <fieldName>.height
+                extX <- max(extX, <fieldName>.extX)
+                extY <- max(extY, <fieldName>.extY)
+                width<- max(width, <fieldName>.width)
+                <fieldName>.isHidden<-false
+            } else {<fieldName>.isHidden<-true}
+    '''.replace('<fieldName>', fieldName)
+      #  updateFuncText+='        if(crntWidth<'+fieldName+'.width){crntWidth <- '+fieldName+'.width}'
+        handleClicksFuncText = '            '+fieldName+'.primaryClick(event)'
+        return [structText, updateFuncText, drawFuncText, setPosFuncText, handleClicksFuncText]
 
 #---------------------------------------------------------------  DUMP MAKING CODE
 
@@ -266,6 +268,48 @@ struct display_'''+className+": inherits = 'dash' "+'''{
 }\n'''
         codeDogParser.AddToObjectFromText(classes[0], classes[1], Code, 'display_'+className)
 
+class structProcessor:
+    addGlobalCode():
+        pass
+
+    processField(fieldName, field, fldCat):
+        pass
+
+    addTheNewClasses():
+        pass
+
+    processStruct(classes, className, dispMode):
+        global classesEncoded
+        cdlog(2, "ENCODING "+dispMide+": "+ className)
+        classesEncoded[className]=1
+        [textFuncBody, structTextAcc, updateFuncTextAcc, drawFuncTextAcc, setPosFuncTextAcc, handleClicksFuncTextAcc, handleClicksFuncTxtAcc2] = ['', '', '', '', '', '', '']
+        updateFuncTextPart2Acc=''; drawFuncTextPart2Acc=''
+        modelRef = progSpec.findSpecOf(classes[0], className, 'model')
+        if modelRef==None:
+            cdErr('To write a dump function for class '+className+' a model is needed but is not found.')
+        ### Write code for each field
+        for field in modelRef['fields']:
+            fldCat=progSpec.fieldsTypeCategory(field['typeSpec'])
+            fieldName=field['fieldName']
+            processField(fieldName, field, fldCat)
+        addTheNewClasses()
+
+class structToStringWriter(structProcessor):
+    addGlobalCode():
+        CODE+="""
+    me string: dispFieldAsText(me string: label, me int:labelLen) <- {
+        me string: S <- ""
+        me int: labelSize<-label.size()
+        withEach count in RANGE(0..labelLen):{
+            if (count<labelSize){S <- S+label[count]}
+            else if(count==labelSize){ S <- S+":"}
+            else {S <- S+" "}
+        }
+        return(S)
+    }
+    """
+
+
 def apply(classes, tags, className, dispMode):
     if dispMode[:4]=='TAG_':
         dispModeTagName=dispMode[4:]
@@ -274,11 +318,8 @@ def apply(classes, tags, className, dispMode):
         cdErr('Invalid parameter for display mode in Code Data Display pattern: '+str(dispMode))
     if dispMode=='none': return
 
-    global classesToProcess
-    global thisPatternAlreadyUsedOnce
-    if(not thisPatternAlreadyUsedOnce):
-        thisPatternAlreadyUsedOnce=True
-        CODE="""
+    global classesToProcess=[]
+    CODE="""
 struct GLOBAL{
     me string: dispBool(me bool: tf) <- {
         if(tf){return("true")} else {return("false")}
@@ -312,6 +353,8 @@ struct GLOBAL{
 
     classesToProcess.append(className)
     for classToEncode in classesToProcess:
+        if(dispMode=='text'):   processor = structToStringWriter()
+        elif(dispMode=='draw'): processor = writeCodeToDrawStructs()
         pattern_GenSymbols.apply(classes, {}, [classToEncode])      # Invoke the GenSymbols pattern
-        EncodeDumpFunction(classes, classToEncode, dispMode)
+        processor.processStruct(classes, classToEncode, dispMode)
     return
