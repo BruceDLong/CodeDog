d02fb4d1f1d2561e2368d4ee115d2a3792e0fd9b
RMartires <rohitmartires14@gmail.com>
2021-06-15 01:37:59 +0530

    feat: added /Scons + modified linux build to use scons

---
+++ Scons/scons-local-4.1.0.post1/SCons/__init__.py
@@ -0,0 +1,9 @@
+__version__="4.1.0.post1"
+__copyright__="Copyright (c) 2001 - 2021 The SCons Foundation"
+__developer__="bdbaddog"
+__date__="2021-05-28 17:53:39"
+__buildsys__="ProDog2020"
+__revision__="12f24df0004ced8b91e758f29cd6396e60f37f5a"
+__build__="12f24df0004ced8b91e758f29cd6396e60f37f5a"
+# make sure compatibility is always in place
+import SCons.compat # noqa
