dc24a0ca92b14155f0319b5aa7f85a402a75019b
Kiran Malvi <kiran.mind2014@gmail.com>
2021-08-18 00:22:54 +0530

    Does it check for a minimum version of libraries? It looks like it ch… (#162)
    * Does it check for a minimum version of libraries? It looks like it checks for an exact match.
    Also, go ahead and put pyParsing, scons, and gitPython in. Are there others that are not installed by default?
    Have you tested? Remove one and see if it reinstalls.
    (DONE)
    
    * Does it check for a minimum version of libraries? It looks like it checks for an exact match.
    Also, go ahead and put pyParsing, scons, and gitPython in. Are there others that are not installed by default?
    Have you tested? Remove one and see if it reinstalls.
    (DONE)
    
    Co-authored-by: Kiran <kiran.malvi@mindinventory.com>
    Co-authored-by: Bruce Long <qstream@gmail.com>

--- checkSys.py
+++ checkSys.py
@@ -1,23 +1,50 @@
 import subprocess
-from pip._internal.utils.misc import get_installed_distributions as getPipList
+import pkg_resources
+# import importlib_metadata
 
-def InstallPipModules():
-    installedModulesList = {"%s==%s" % (i.key, i.version) for i in getPipList()}
-    requiredModulesList = {'mutagen==1.45.1', 'gtts==2.2.3', 'pyproj==3.1.0'}
-    modulesList = requiredModulesList.difference(installedModulesList)
+def DownloadInstallPipModules(pipCMD):
+    pipe = subprocess.Popen(pipCMD, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = pipe.communicate()
+    decodedOut = bytes.decode(err)
+    if err:
+        print("\n\n", decodedOut)
+
+def CheckPipModules():
+    requiredMinimumModulesList = {'pyparsing':'3.0.0b2', 'GitPython':'3.1.18', 'scons':'4.2.0', 'pycurl':'7.43.0.6'}
+    modulesList = []
+    for moduleName in requiredMinimumModulesList:
+        try:
+            # version = importlib_metadata.version(moduleName)
+            version = pkg_resources.get_distribution(moduleName).version
+        except:
+            modulesList.append("%s==%s" % (moduleName, requiredMinimumModulesList[moduleName]))
+        else:
+            version = version.split(".")
+            installedModuleVersion = float(".".join(version[:2]))
+            moduleVersion = requiredMinimumModulesList[moduleName].split(".")
+            requiredModuleVersion = float(".".join(moduleVersion[:2]))
+            if not installedModuleVersion >= requiredModuleVersion:
+                modulesList.append("%s==%s" % (moduleName, requiredMinimumModulesList[moduleName]))
+
     if modulesList:
         print("CodeDog must be used with Python Modules")
         for module in modulesList:
             print("     %s" % module)
-        # ModulesList = " ".join(ModulesList)
 
         installationPermission = input("Do you want to install? [Y/n] ")
-        if installationPermission.lower()=='y' or installationPermission.lower()=='yes':
+        if installationPermission.lower() == 'y' or installationPermission.lower() == 'yes':
             for module in modulesList:
-                print("Installing collected packages: ", module)
-                subprocess.check_call(['pip', 'install', '-q', module, '--disable-pip-version-check'])
+                print("\nInstalling collected packages: ", module)
+                pipCMD = 'pip install -q %s --disable-pip-version-check' % module
+                DownloadInstallPipModules(pipCMD)
+
         else:
             print("\n\nERROR: CodeDog must be used with python modules\n")
             for module in modulesList:
                 print("     %s" % module)
             exit(1)
+
+def AddSystemPath():
+    addPathPermission = input("Do you want to add CodeDog to System Path? [Y/n] ")
+    if addPathPermission.lower() == 'y' or addPathPermission.lower() == 'yes':
+        pass
