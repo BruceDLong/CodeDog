c7b08b8e8c3f547146feb3fed9f3fdfbd80d9204
Kiran <kiran.malvi@mindinventory.com>
2021-08-23 18:53:59 +0530

    set environment variable path for all platform.

--- checkSys.py
+++ checkSys.py
@@ -3,13 +3,34 @@ import pkg_resources
 import os
 
 def AddSystemPath():
+    from sys import platform
     pathList = os.get_exec_path()
     codeDogPath = os.path.dirname(os.path.realpath(__file__))
     if codeDogPath in pathList: return
     addPathPermission = input("Do you want to add CodeDog to the System Path? [Y/n] ")
     if addPathPermission.lower() == 'y' or addPathPermission.lower() == 'yes' or addPathPermission == '':
         # Research how to permanently set the path for Linux, Mac, Windows via python3
-        pass
+        if platform == "linux" or platform == "linux2":
+            bashfile = os.path.join(os.path.expanduser('~'), '.bashrc')
+            with open(bashfile, 'a') as f:
+                f.write('\nPATH="$PATH:$HOME/devl/CodeDog"\n')
+
+        elif platform == "darwin":
+            zshrcfile = os.path.join(os.path.expanduser('~'), '.zshrc')
+            with open(zshrcfile, 'a') as f:
+                f.write('\nexport PATH="$PATH:$HOME/devl/CodeDog"\n')
+
+        elif platform == "win32" or platform == "win64":
+            import winreg as reg
+            key = reg.OpenKey(reg.HKEY_CURRENT_USER, 'Environment', 0, reg.KEY_ALL_ACCESS)
+            try:
+                value, _ = reg.QueryValueEx(key, 'PATH')
+            except WindowsError:
+                value = ''
+            codeDogPath = os.getcwd()
+            value += ";" + codeDogPath + "\CodeDog;"
+            reg.SetValueEx(key, 'PATH', 0, reg.REG_EXPAND_SZ, value)
+            reg.CloseKey(key)
     else:
         print("If you wish to add CodeDog to the system path manually, it is:\n    ", codeDogPath)
 
