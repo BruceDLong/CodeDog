d06c4f32a0e322a412e9eb13223ee78da7ffef4b
Kiran <kiran.malvi@mindinventory.com>
2021-08-24 11:57:10 +0530

    set environment variable path for all platform with check existing path.

--- checkSys.py
+++ checkSys.py
@@ -12,25 +12,43 @@ def AddSystemPath():
         # Research how to permanently set the path for Linux, Mac, Windows via python3
         if platform == "linux" or platform == "linux2":
             bashfile = os.path.join(os.path.expanduser('~'), '.bashrc')
-            with open(bashfile, 'a') as f:
-                f.write('\nPATH="$PATH:$HOME/devl/CodeDog"\n')
+            sytemflag = 0
+            systemPath = 'PATH="$PATH:$HOME/devl/CodeDog"'
+            with open(bashfile, 'r') as f:
+                if systemPath in f.read():
+                    sytemflag = 1
+
+            if not sytemflag:
+                with open(bashfile, 'a') as f:
+                    f.write('\n%s\n' %systemPath)
 
         elif platform == "darwin":
+            sytemflag = 0
             zshrcfile = os.path.join(os.path.expanduser('~'), '.zshrc')
-            with open(zshrcfile, 'a') as f:
-                f.write('\nexport PATH="$PATH:$HOME/devl/CodeDog"\n')
+            systemPath = 'export PATH="$PATH:$HOME/devl/CodeDog"'
+            with open(zshrcfile, 'a+') as f:
+                if systemPath in f.read():
+                    sytemflag = 1
+
+            if not sytemflag:
+                with open(zshrcfile, 'a+') as f:
+                    f.write('\n%s\n' %systemPath)
 
         elif platform == "win32" or platform == "win64":
+            sytemflag = 0
             import winreg as reg
             key = reg.OpenKey(reg.HKEY_CURRENT_USER, 'Environment', 0, reg.KEY_ALL_ACCESS)
             try:
                 value, _ = reg.QueryValueEx(key, 'PATH')
             except WindowsError:
                 value = ''
-            codeDogPath = os.getcwd()
-            value += ";" + codeDogPath + "\CodeDog;"
-            reg.SetValueEx(key, 'PATH', 0, reg.REG_EXPAND_SZ, value)
-            reg.CloseKey(key)
+            if 'CodeDog' in value:
+                sytemflag = 1
+            if not sytemflag:
+                codeDogPath = os.getcwd()
+                value += ";" + codeDogPath + "\CodeDog;"
+                reg.SetValueEx(key, 'PATH', 0, reg.REG_EXPAND_SZ, value)
+                reg.CloseKey(key)
     else:
         print("If you wish to add CodeDog to the system path manually, it is:\n    ", codeDogPath)
 
