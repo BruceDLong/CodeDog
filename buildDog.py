e5ef6f9b63653c776b1363c8d1f938b909d0a273
pyr0ball <pyroballpcs@gmail.com>
2022-10-18 15:18:10 -0700

    Revert "added packageManager 'sys' handling to tags"
    This reverts commit 7afb141c940c83162ff503da8945d9b86e6a25a0.

--- buildDog.py
+++ buildDog.py
@@ -204,7 +204,6 @@ def fetchPackages(packageData, packageDirectory):
         if fetchType == "git":    gitClone(fetchURL, packageName, packageDirectory)
         elif fetchType == "file": downloadPackageFile(fetchURL, packageName, packageDirectory)
         elif fetchType == "zip":  downloadExtractZip(fetchURL, packageName, packageDirectory)
-        elif fetchType == "sys": checkAndUpgradeOSPackageVersions(packageName)
         else: pass
 
 def FindOrFetchLibraries(buildName, packageData, platform, tools):
