9ea44b71930c53c492c0870544228b983814ba3a
pyr0ball <pyroballpcs@gmail.com>
2022-10-28 15:20:52 -0700

    refactored package handling with additional loops and concatination

--- environmentMngr.py
+++ environmentMngr.py
@@ -18,115 +18,176 @@ def checkToolWindows(toolName):
 
 def findPackageManager():
     installedPackageManagerList = []
-    packageManagers = ["dpkg", "brew", "yum", "gdebi", "apt-get", "pacman", "emerge", "zypper", "dnf"]
+    packageManagers = ["dpkg", "brew", "yum", "gdebi", "apt-get", "pacman", "emerge", "zypper", "dnf", "rpm"]
 
     for pmgr in packageManagers:
         if checkToolLinux(pmgr):
             installedPackageManagerList.append(pmgr)
     return installedPackageManagerList
 
-def packageInstall(packageManager, pmgrInstallFlags, packageName):
+def setPackageMgrFlags(packageManager):
+    # TODO: Set up arrays for multiple package managers [e.g. pkgMgr1Install(apt-get) mkgMgr2Install(gdebi)]
+    # TODO: For now, Need to ensure only one match is made. I believe this will loop and select the last one returned. 
+    # There should be some heirarchy for choosing between these depending on the detected OS. We could even
+    # make this a user choice
+    pm = {packageManager}
+    if pm == 'dpkg':
+        pmgrPrepend      = "echo yes | sudo "
+        pmgrInstallFlags = "-i"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'apt-get':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "install -y"
+        # TODO: need to either create a separate workflow for apt-get/apt-cache or we need to use apt instead as the original binary does not have query function
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "remove"
+        pmgrUpgradeFlags = "upgrade"
+    elif pm == 'gdebi':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-i"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'rpm':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-i"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'yum':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-i"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'pacman':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-S"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'dnf':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "install -y"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "remove"
+        pmgrUpgradeFlags = "upgrade"
+    elif pm == 'emerge':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-pv"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'zypper':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "-i"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+    elif pm == 'brew':
+        pmgrPrepend      = "sudo "
+        pmgrInstallFlags = "install --cask"
+        pmgrCurrentFlags = "-l"
+        pmgrRemoveFlags  = "-r"
+        pmgrUpgradeFlags = "-i"
+
+    return pmgrPrepend,pmgrInstallFlags,pmgrCurrentFlags,pmgrRemoveFlags,pmgrUpgradeFlags
+
+def packageInstall(packageManager, packageName):
+    setPackageMgrFlags({packageManager})
     cdlog(1, "Package Installing: "+packageName)
-    if subprocess.call(f'{packageManager} {pmgrInstallFlags} {packageName}'+" > /dev/null 2>&1", shell=True) == 0:
+    if subprocess.call(f'{pmgrPrepend} {packageManager} {pmgrInstallFlags} {packageName}'+" > /dev/null 2>&1", shell=True) == 0:
         cdlog(1, "Package installed Successfully")
         return True
     else:
         cdErr("Unable to install package. \nPlease install manually : " + packageName)
 
-def packageRemove(packageManager, pmgrRemoveFlags, packageName):
+def packageRemove(packageManager, packageName):
+    setPackageMgrFlags({packageManager})
     cdlog(1, "Package Installing: "+packageName)
-    if subprocess.call(f'{packageManager} {pmgrRemoveFlags} {packageName}'+" > /dev/null 2>&1", shell=True) == 0:
-        cdlog(1, "Package installed Successfully")
+    if subprocess.call(f'{pmgrPrepend} {packageManager} {pmgrRemoveFlags} {packageName}'+" > /dev/null 2>&1", shell=True) == 0:
+        cdlog(1, "Package removed Successfully")
         return True
     else:
-        cdErr("Unable to install package. \nPlease install manually : " + packageName)
+        cdErr("Unable to remove package. \nPlease remove manually : " + packageName)
 
-def packageInstalled(packageManager, pmgrCurrentFlags, packageName):
+def packageInstalled(packageManager, packageName):
+    setPackageMgrFlags({packageManager})
     cdlog(1, "Checking for installed Package: "+packageName)
-    if subprocess.call(f'{packageManager} {pmgrCurrentFlags} {packageName}'+ "> /dev/null 2>&1", shell=True) == 0:
+    if subprocess.call(f'{pmgrPrepend} {packageManager} {pmgrCurrentFlags} {packageName}'+ "> /dev/null 2>&1", shell=True) == 0:
         cdlog(1, "Package Is Currently Installed")
         return True
     else:
         cdlog(1, "Package Is NOT Currently Installed")
         return False
 
-
-def setPackageMgrFlags(installedPackageManagerList):
-    for ipm in installedPackageManagerList:
-        if ipm == 'dpkg' and packageExtension == 'deb':
-            pmgrInstallFlags = "-i"
-            pmgrCurrentFlags = "-l"
-            pmgrRemoveFlags  = "-r"
-            break
-        elif ipm == 'apt-get':
-            if packageInstall("sudo apt-get install", packageName):
-                break
-        elif ipm == 'gdebi':
-            if packageInstall("sudo gdebi --apt-line", packageName):
-                break
-        elif ipm == 'yum':
-            if packageInstall("sudo yum install", packageName):
-                break
-        elif ipm == 'pacman':
-            if packageInstall("sudo pacman -S", packageName):
-                break
-        elif ipm == 'dnf':
-            if packageInstall("sudo dnf install", packageName):
-                break
-        elif ipm == 'emerge':
-            if packageInstall("sudo emerge -pv", packageName):
-                break
-        elif ipm == 'zypper':
-            if packageInstall("sudo zypper install", packageName):
-                break
-        elif ipm == 'brew':
-            if packageInstall("brew install --cask", packageName):
-                break
+def packageUpdate(packageManager, packageName):
+    setPackageMgrFlags({packageManager})
+    cdlog(1, "Package Updating: "+packageName)
+    if subprocess.call(f'{pmgrPrepend} {packageManager} {pmgrUpdateFlags} {packageName}'+" > /dev/null 2>&1", shell=True) == 0:
+        cdlog(1, "Package updated Successfully")
+        return True
+    else:
+        cdErr("Unable to update package. \nPlease try manually : " + packageName)
 
 def getPackageManagerCMD(packageName, installedPackageManagerList):
     packageExtension = packageName.split(".")[-1]
     for ipm in installedPackageManagerList:
-        if ipm == 'dpkg' and packageExtension == 'deb':
-            if packageInstall("sudo dpkg -i", packageName):
+        if ipm == 'gdebi' and packageExtension == 'deb':
+            return "gdebi"
+            # if packageInstall("gdebi", packageName):
+            #     break
+        elif ipm == 'dpkg' and packageExtension == 'deb':
+            if packageInstall("dpkg", packageName):
+                break
+        elif ipm == 'rpm' and packageExtension == 'rpm':
+            if packageInstall("rpm", packageName):
                 break
         elif ipm == 'apt-get':
-            if packageInstall("sudo apt-get install", packageName):
-                break
-        elif ipm == 'gdebi':
-            if packageInstall("sudo gdebi --apt-line", packageName):
+            if packageInstall("apt-get", packageName):
                 break
         elif ipm == 'yum':
-            if packageInstall("sudo yum install", packageName):
+            if packageInstall("yum", packageName):
                 break
         elif ipm == 'pacman':
-            if packageInstall("sudo pacman -S", packageName):
+            if packageInstall("pacman", packageName):
                 break
         elif ipm == 'dnf':
-            if packageInstall("sudo dnf install", packageName):
+            if packageInstall("dnf", packageName):
                 break
         elif ipm == 'emerge':
-            if packageInstall("sudo emerge -pv", packageName):
+            if packageInstall("emerge", packageName):
                 break
         elif ipm == 'zypper':
-            if packageInstall("sudo zypper install", packageName):
+            if packageInstall("zypper", packageName):
                 break
         elif ipm == 'brew':
-            if packageInstall("brew install --cask", packageName):
+            if packageInstall("brew", packageName):
                 break
-
 
 def checkAndUpgradeOSPackageVersions(packageName):
     cdlog(1, f"Searching for package: {packageName}")
-    installedPackage = os.popen(f'apt-cache policy {packageName} | grep Installed')
-    if installedPackage.read():
-        installedPackage = os.popen(f'apt-cache policy {packageName} | grep Installed')
-        candidatePackage = os.popen(f'apt-cache policy {packageName} | grep Candidate')
-        installedVersion = installedPackage.read().split(" ")[-1].replace('\n','')
-        candidateVersion = candidatePackage.read().split(" ")[-1].replace('\n','')
+    pmgr = getPackageManagerCMD()
+    setPackageMgrFlags(pmgr)
+    # Uses a concatanated string from the package manager and flags, returns a boolean result for positive or negative [0|1]
+    # installedPackage = os.popen(f'{pmgr} {pmgrCurrentFlags} {packageName} > /dev/null 2>&1 ; echo -e $?')
+    currentlyInstalled = packageInstalled({pmgr} {packageName})
+    # If 0 (detected as installed):
+    if currentlyInstalled == 'True':
+        # Pull base label for currently installed package version0
+        _installedPackage = os.popen(f'{pmgr} {pmgrCurrentFlags} {packageName} | grep -i Installed')
+        # Pull base label for candidate installer version
+        _candidatePackage = os.popen(f'{pmgr} {pmgrCurrentFlags} {packageName} | grep -i Candidate')
+        # Parse version number from base labels
+        installedVersion = _installedPackage.read().split(" ")[-1].replace('\n','')
+        candidateVersion = _candidatePackage.read().split(" ")[-1].replace('\n','')
         cdlog(1, f"Candidate Package available: {candidateVersion}")
+        # Compare versions and apply updates only if needed
         if installedVersion or candidateVersion == '(none)':
             if installedVersion != candidateVersion:
-                getPackageManagerCMD(packageName, findPackageManager())
+                packageInstall(packageName, pmgr)
             else:
                 cdlog(1, f"Package already Installed: {packageName}")
     else:
@@ -158,7 +219,7 @@ def installPipPackage():
     downloadUrl = "https://bootstrap.pypa.io/get-pip.py"
     fileName = "get-pip.py"
     
-    if platform == "linux" or platform == "linux2":
+    if platform == "linux" or platform == "linux2" or platform == "linux-gnu":
         if not checkToolLinux(toolName):
             getPackageManagerCMD('python3-pip', findPackageManager()) # Install PIP3
 
