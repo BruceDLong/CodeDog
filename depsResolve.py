256fff18abb53c83b537ed45f5d5fccefe6c3685
pyr0ball <pyroballpcs@gmail.com>
2022-11-15 14:17:45 -0800

    added files missed during last commit

--- depsResolve.py
+++ depsResolve.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import subprocess
+import os
+import sys
+from progSpec import cdErr, cdlog
+from pmgrHandler import findPackageManager
 
 def checkToolLinux(toolName):
     if subprocess.call(["which", toolName], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
@@ -12,16 +16,44 @@ def checkToolWindows(toolName):
         return True
     else:
         return None
-      
+
+def downloadFile(fileName, downloadURL):
+    import urllib3
+    try:
+        cdlog(1, "Downloading file: " + fileName)
+        http = urllib3.PoolManager()
+        r = http.request('GET', downloadURL, preload_content=False)
+    except:
+        cdErr("URL not found: " + downloadURL)
+    else:
+        with open(fileName, 'wb') as out:
+            while True:
+                data = r.read(1028)
+                if not data:
+                    break
+                out.write(data)
+        r.release_conn()
+
+def packageInstall(packageName):
+    from pmgrHandler import getPackageManagerCMD
+    pmgrCMD = getPackageManagerCMD(packageName, findPackageManager(),"install")
+    cdlog(1, "Package Installing: "+packageName)
+    if subprocess.call(f'{pmgrCMD}'+" > /dev/null 2>&1", shell=True) == 0:
+        cdlog(1, "Package installed Successfully")
+        return True
+    else:
+        cdErr("Unable to install package. \nPlease install manually : " + packageName)
+
 def installPipPackage():
     from sys import platform
+    
     toolName = "pip3"
     downloadUrl = "https://bootstrap.pypa.io/get-pip.py"
     fileName = "get-pip.py"
     
     if platform == "linux" or platform == "linux2" or platform == "linux-gnu":
         if subprocess.call(["which", "pip3"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) != 0:
-            checkAndUpgradeOSPackageVersions('python3-pip') # Install PIP3
+            packageInstall('python3-pip') # Install PIP3
         # If package manager fails to install, try using the bootstrap script
         if not checkToolLinux(toolName):
             downloadFile(fileName, downloadUrl)
@@ -37,13 +69,7 @@ def installPipPackage():
             downloadFile(fileName, downloadUrl)
             os.system('py get-pip.py') # Install PIP3
 
-
-    print("pyparsingmodule not found")
-    if subprocess.call(["which", "pip3"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0:
-        if subprocess.call(f'pip3 install pyparsing > /dev/null 2>&1', shell=True) == 0:
-            print("Installed PyParsing Module")
-    else:
-        print("python3-pip is not currently installed")
-        from environmentMngr import installPipPackage
-        installPipPackage()
-        subprocess.call(f'python3 -m pip install pyparsing > /dev/null 2>&1', shell=True)
+def installPyparsing():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyparsing"])
+    
+        
